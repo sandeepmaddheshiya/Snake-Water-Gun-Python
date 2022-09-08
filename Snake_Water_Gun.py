65a02c75dd15de4e95360d880583d721b0304318
Sandeep Maddheshiya <113048079+Itz-Sandy@users.noreply.github.com>
2022-09-08 21:19:36 +0530

    Add files via upload

--- Snake_Water_Gun.py
+++ Snake_Water_Gun.py
@@ -67,7 +67,7 @@ while(True):
     if s>c:
         print("CONGRATULATIONS! YOU'VE WON THE GAME ")
     elif s==c:
-        print("GAME DRAW")
+        print("GAME TIE")
     else:
         print("BETTER TRY NEXT TIME")
     print("----------------GAME OVER---------------------")
